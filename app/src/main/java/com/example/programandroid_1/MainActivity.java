e61924e1d21241a58b0af68d79e3b35e611690ac
LeluRezt <lelurezt@gmail.com>
2020-10-01 18:51:47 +0700

    Add code at line 13

--- app/src/main/java/com/example/programandroid_1/MainActivity.java
+++ app/src/main/java/com/example/programandroid_1/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.programandroid_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("13", "Debugging");
         setContentView(R.layout.activity_main);
     }
 }
