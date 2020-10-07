074c704d89cb6e213443059bc62068d4ac890cd4
LeluRezt <lelurezt@gmail.com>
2020-10-07 08:55:27 +0700

    Add new features

--- app/src/main/java/com/example/programandroid_1/MainActivity.java
+++ app/src/main/java/com/example/programandroid_1/MainActivity.java
@@ -2,15 +2,41 @@ package com.example.programandroid_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_login;
+    EditText ed_username, ed_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("13", "Debugging");
         setContentView(R.layout.activity_main);
+        btn_login = findViewById(R.id.btn_login);
+        ed_username = findViewById(R.id.ed_username);
+        ed_password = findViewById(R.id.ed_password);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = ed_username.getText().toString().trim();
+                String password = ed_password.getText().toString().trim();
+
+                if (username.equals("admin") && password.equals("admin")){
+                    Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "username atau password anda tidak benar!", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
     }
 }
