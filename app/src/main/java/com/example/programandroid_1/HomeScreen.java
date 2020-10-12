dbe37fb13a636a94c669521af402a83de1771f25
LeluRezt <lelurezt@gmail.com>
2020-10-12 12:05:06 +0700

    Add Tab Layout

--- app/src/main/java/com/example/programandroid_1/HomeScreen.java
+++ app/src/main/java/com/example/programandroid_1/HomeScreen.java
@@ -3,6 +3,7 @@ package com.example.programandroid_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity {
@@ -11,6 +12,7 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        Log.d("msg","msg");
         Toast.makeText(getApplicationContext(), "Selamat anda berhasil login", Toast.LENGTH_SHORT)
                 .show();
     }
