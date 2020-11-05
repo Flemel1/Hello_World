afd5b63f7a6659130001eab5dee40e882cd8205b
danielelonu <daniel.lelonu@ti.ukdw.ac.id>
2020-11-05 22:13:30 +0700

    Added job scheduler and recyclerview

---
+++ app/src/main/java/com/example/programandroid_1/data/RecordItem.java
@@ -0,0 +1,31 @@
+package com.example.programandroid_1.data;
+
+public class RecordItem {
+    private int idImage;
+    private String title;
+    private String description;
+
+    public int getIdImage() {
+        return idImage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setIdImage(int idImage) {
+        this.idImage = idImage;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- app/src/main/java/com/example/programandroid_1/fragment/FragmentTop.java
+++ app/src/main/java/com/example/programandroid_1/fragment/FragmentTop.java
@@ -1,16 +1,30 @@
 package com.example.programandroid_1.fragment;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.programandroid_1.MyJobService;
 import com.example.programandroid_1.R;
 
 
-public class FragmentTop extends Fragment {
+public class FragmentTop extends Fragment implements View.OnClickListener {
+
+    private final String TAG = "FragmentTop";
+
+    private Button btnStartJob;
+    private Button btnStopJob;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,5 +35,49 @@ public class FragmentTop extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        btnStartJob = view.findViewById(R.id.btn_start_job);
+        btnStopJob = view.findViewById(R.id.btn_stop_job);
+        btnStartJob.setOnClickListener(this);
+        btnStopJob.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_start_job : startedJob(); break;
+            case R.id.btn_stop_job : stoppedJob(); break;
+        }
+    }
+
+    private void stoppedJob() {
+        JobScheduler scheduler = (JobScheduler) getActivity().getSystemService(
+                Context.JOB_SCHEDULER_SERVICE
+        );
+        scheduler.cancel(123);
+        Log.i(TAG, "Job cancelled");
+    }
+
+    private void startedJob() {
+        ComponentName componentName = new ComponentName(getContext().getApplicationContext(), MyJobService.class);
+        JobInfo toastInfo = new JobInfo.Builder(123, componentName)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15 * 60 * 1000)
+                .build();
+        JobScheduler scheduler = (JobScheduler) getActivity().getSystemService(
+                Context.JOB_SCHEDULER_SERVICE
+                 );
+        int resultCode = scheduler.schedule(toastInfo);
+        if (resultCode == JobScheduler.RESULT_SUCCESS) {
+            Log.i(TAG, "Job Scheduled");
+        }
+        else {
+            Log.i(TAG, "Job scheduling failed");
+        }
     }
 }
